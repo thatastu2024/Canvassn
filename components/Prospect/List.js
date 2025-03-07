bca4bd943456ce1bac7df27cebf23b9fd7867288
Suraj Tiwari <st143693@gmail.com>
2025-03-07 14:36:37 +0530

    Canvassn-12_Working_on_search_prospect

--- components/Prospect/List.js
+++ components/Prospect/List.js
@@ -1,6 +1,9 @@
 import  {React, useState, useEffect } from "react";
 import axios from 'axios';
 import {formatDateTime} from '../../utils/dateUtil'
+import { faArrowDown,faArrowUp} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { Input } from "postcss";
 
 export default function ProspectList() {
   const [error, setError] = useState("");
@@ -8,10 +11,20 @@ export default function ProspectList() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-        try {
+    fetchData()
+  }, []);
+  const fetchData = async (filterField=null,filterValue=null) => {
+      try {
+        if(filterField === null && filterValue === null){
+          filterField='createdAt',
+          filterValue='-1'
+        }
         let token=localStorage.getItem('token')
         const response=await axios.get('/api/prospects',{
+          params:{
+            filterField,
+            filterValue
+          },
           headers:{
               Authorization:'Bearer '+token,
               "Content-Type": "application/json",
@@ -21,15 +34,13 @@ export default function ProspectList() {
             throw new Error("Failed to fetch data");
           }
           setProspects(response?.data?.data);
-        } catch (error) {
-          console.log(error)
-          setError(error.message);
-        } finally {
-          setLoading(false);
-        }
-    };
-    fetchData()
-  }, []);
+      } catch (error) {
+        console.log(error)
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+  };
   if (loading) {
     return (
       <div className="flex items-center justify-center min-h-screen">
@@ -47,20 +58,36 @@ export default function ProspectList() {
           <h2 className="text-xl font-semibold mb-4">Prospects</h2>
         </div>
         <div className="overflow-x-auto">
+          <div className="flex justify-end mb-4">
+            <label className="px-6 py-2 text-lg font-medium text-gray-700">Search</label>
+            <input className="border rounded px-6 py-2 text-gray-600 mr-2" type="text" onKeyUp={e=>{fetchData('search',e.target.value)}}>
+            </input>
+            <select className="border rounded px-3 py-2 text-gray-600">
+              <option>All Customers</option>
+            </select>
+          </div>
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-50">
                 <tr>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Name
+                    <FontAwesomeIcon icon={faArrowUp} className="ml-2" onClick={e=>{fetchData('name','-1')}}/>
+                    <FontAwesomeIcon icon={faArrowDown} onClick={e=>{fetchData('prospect_name','1')}}/>
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Email
+                    <FontAwesomeIcon icon={faArrowUp} className="ml-2" onClick={e=>{fetchData('prospect_email','-1')}}/>
+                    <FontAwesomeIcon icon={faArrowDown} onClick={e=>{fetchData('prospect_email','1')}}/>
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Location
+                    <FontAwesomeIcon icon={faArrowUp} className="ml-2" onClick={e=>{fetchData('prospect_email','-1')}}/>
+                    <FontAwesomeIcon icon={faArrowDown} onClick={e=>{fetchData('location','1')}}/>
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Created At
+                    <FontAwesomeIcon icon={faArrowUp} className="ml-2" onClick={e=>{fetchData('createdAt','-1')}}/>
+                    <FontAwesomeIcon icon={faArrowDown} onClick={e=>{fetchData('createdAt','1')}}/>
                     </th>
                 </tr>
             </thead>
