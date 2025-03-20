993b0becfd5a2381a1390adc195b34d872682ff2
Suraj Tiwari <35627146+Suraj54321@users.noreply.github.com>
2025-03-20 23:45:26 +0530

    Update index.js

--- pages/api/prospects/index.js
+++ pages/api/prospects/index.js
@@ -11,10 +11,10 @@ export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Credentials", "true");
 
     if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
         return res.status(200).json({status:'OK'})
     }
 
