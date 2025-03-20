76c1a836b53955067b645a9d138c02cf6c265291
Suraj Tiwari <st143693@gmail.com>
2025-03-21 00:18:41 +0530

    Canvassn_19_Solved_Cors_issues

--- pages/api/prospects/index.js
+++ pages/api/prospects/index.js
@@ -26,14 +26,10 @@ function runMiddleware(req, res, fn) {
 
 export default async function handler(req, res) {
     await runMiddleware(req, res, cors);
-    // res.setHeader("Access-Control-Allow-Origin", "*");
-    // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    // res.setHeader("Access-Control-Allow-Credentials", "true");
 
     if (req.method === "OPTIONS") {
         res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type");
         return res.status(200).json({status:'OK'})
     }
@@ -59,10 +55,6 @@ export default async function handler(req, res) {
             }
         }
         const prospects = await Prospects.find({},"_id, prospect_name prospect_email prospect_location createdAt").sort(sortData);
-        res.headers("Access-Control-Allow-Origin", "*");
-        res.headers("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.headers("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        res.headers("Access-Control-Allow-Credentials", "true");
         return res.status(200).json({ success: true, data: prospects });
       } catch (error) {
         return res.status(500).json({ success: false, error: error.message });
@@ -90,11 +82,6 @@ export default async function handler(req, res) {
                 }).lean()
                 if(checkUserExists !== null && checkUserExists.hasOwnProperty('_id')){
                     const token = generateToken(checkUserExists)
-                    res.header("Access-Control-Allow-Origin", "*");
-                    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-                    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-                    res.header("Access-Control-Allow-Credentials", "true");
-                    console.log(res)
                     return res.status(201).json({ success: true, message: "Prospect Authenticated Successfully",data:{
                         prospect_id:checkUserExists._id,
                         prospectToken:token
@@ -112,7 +99,6 @@ export default async function handler(req, res) {
                         status: 'active'
                     }
                     let data=await Prospects.create(finalResponse);
-                    console.log(data._id)
                     if(data) {
                         const token = generateToken(data)
                         return res.status(201).json({ success: true, message: "Prospect record captured successfully",data:{
@@ -129,8 +115,6 @@ export default async function handler(req, res) {
           return res.status(400).json({ success: false, error: error.message });
         }
       }
-    
-    //   return res.status(405).json({ success: false, error: "Method Not Allowed" });
 }
 
 // authMiddleware(handler)
