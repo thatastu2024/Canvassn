a3916c14d1103b622fa4d04cecc43e592127fce1
Suraj Tiwari <35627146+Suraj54321@users.noreply.github.com>
2025-03-21 00:12:16 +0530

    Update canvassn-chat-widget.js

--- public/canvassn-chat-widget.js
+++ public/canvassn-chat-widget.js
@@ -599,10 +599,7 @@ class PublicScript {
         fetch("https://ai-voice-bot-mauve.vercel.app/api/prospects", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin":"*",
-                "Access-Control-Allow-Methods":"GET,DELETE,PATCH,POST,PUT",
-                "Access-Control-Allow-Headers":"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({ 
                 agent_id:this.agentId,
